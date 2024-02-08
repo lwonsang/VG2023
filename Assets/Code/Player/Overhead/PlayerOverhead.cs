b44516099f1f00d768ee5c5fde8da495487f6d90
ljzhou88 <l.j.zhou@wustl.edu>
2024-02-08 17:58:59 -0600

    input and overhead

--- Assets/Code/Player/Overhead/PlayerOverhead.cs
+++ Assets/Code/Player/Overhead/PlayerOverhead.cs
@@ -1,16 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerOverhead : MonoBehaviour
 {
+    #region Variables
+    [Header("Define whether this is first player or not")]
+    public bool is_player_one;
+
+    public Vector2 MovementVector;
+
+    #endregion
+
     void Start()
     {
-            
+        if(is_player_one)
+        {
+
+        }
     }
 
     void FixedUpdate()
     {
         
     }
+
+    public void Movement(InputAction.CallbackContext value)
+    {
+        MovementVector = value.ReadValue<Vector2>();
+    }
+
+
+
 }
