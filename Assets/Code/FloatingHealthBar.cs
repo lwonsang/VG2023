dae5e9316055a1bde9ffe1603451bc91037e1cb2
Eric Biernacki <ebiernacki@wustl.edu>
2024-02-09 18:52:00 -0600

    Added temp player and health bar

---
+++ Assets/Code/FloatingHealthBar.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+namespace Main{
+    public class FloatingHealthBar : MonoBehaviour
+    {
+
+        [SerializeField] private Slider slider;
+
+        public void UpdateHealthBar(float currentValue, float maxValue){
+            slider.value = currentValue / maxValue;
+        }
+
+        // Start is called before the first frame update
+        // void Start()
+        // {
+            
+        // }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // slider.value
+        }
+    }
+
+}
--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -5,15 +5,48 @@ using UnityEngine;
 namespace Main{
     public class PlayerMovement : MonoBehaviour
     {
+
+        public float currentHealth = 10f;
+        public float maxHealth = 10f;
+
+        [SerializeField] FloatingHealthBar healthBar;
+
+
+
+        //broken code: wont print when there is a collision????
+        void OnTriggerEnter2D(Collider2D other){
+            if (other.gameObject.GetComponent<TankController>()){
+                print("Collision with Enemy");
+                // currentHealth -= 1;
+
+                // print("Current Health:" + currentHealth);
+                // healthBar.UpdateHealthBar(currentHealth, maxHealth);
+            }
+            
+        }
+        void OnTriggerExit2D(Collider2D other) {
+            print("Exit Collision with " + other.gameObject.name);
+        }
+
         // Start is called before the first frame update
         void Start()
         {
-            
+            currentHealth = maxHealth;
+            healthBar = GameObject.Find("HealthBar").GetComponentInChildren<FloatingHealthBar>();
         }
 
         // Update is called once per frame
         void Update()
         {
+
+            //temp
+            if (Input.GetMouseButtonDown(0)) {
+                print("Space pressed");
+                currentHealth -= 1;
+
+                print("Current Health:" + currentHealth);
+                healthBar.UpdateHealthBar(currentHealth, maxHealth);
+            }
 
             //Slowed down movement from 0.2f
             //Move Up
