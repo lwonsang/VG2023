a64e28cc750be61c6aaf1cd625b859f74af67ac4
Jason L <35697422+blusoldier001@users.noreply.github.com>
2024-02-23 10:04:09 -0600

    rudimentary grid setup

--- Assets/Code/Testing.cs
+++ Assets/Code/Testing.cs
@@ -7,7 +7,7 @@ public class Testing : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GridMap grid = new GridMap(20, 10);
+        GridMap grid = new GridMap(50, 50, 2f);
     }
 
     // Update is called once per frame
