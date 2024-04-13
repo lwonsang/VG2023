3d3d141f3db5114afbde0ebb2c20cc75b4e41d97
lwonsang <l.wonsang@wustl.edu>
2024-04-12 23:19:36 -0500

    background music manager, bug fixes

--- Assets/Code/Environment/CheckpointFlag.cs
+++ Assets/Code/Environment/CheckpointFlag.cs
@@ -12,6 +12,7 @@ public class CheckpointFlag : MonoBehaviour
     AudioSource _AudioSource;
     public AudioClip checkpointFlagCleared;
     public AudioClip checkpointFlagMissed;
+    private bool complete = false;
 
     private void Start()
     {
@@ -30,13 +31,15 @@ public class CheckpointFlag : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.transform.parent.name == "Player" && allRespawnPoints.Length==0 && allEnemies.Length == 0)
+        if (other.transform.parent.name == "Player" && allRespawnPoints.Length == 0 && allEnemies.Length == 0 &&
+            complete == false) 
         {
             print("Reached checkpoint with all enemies defeated");
             _animator.SetTrigger("CheckpointClearedWave");
             _AudioSource.PlayOneShot(checkpointFlagCleared);
+            complete = true;
         }
-        else if (other.transform.parent.name == "Player")
+        else if (other.transform.parent.name == "Player" && complete == false)
         {
             print("All enemies not defeated");
             _AudioSource.PlayOneShot(checkpointFlagMissed);
